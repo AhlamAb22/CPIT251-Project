123fb6cf448a652c60eb2969898ce63f9ea35611
Danah677 <Alharbidanah213@gmail.com>
2025-12-02 17:59:53 +0300

    Update BookingManager.java

--- src/cpit251/project/BookingManager.java
+++ src/cpit251/project/BookingManager.java
@@ -1,1 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class BookingManager {
+
+    private List<Booking> bookings = new ArrayList<>();
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void viewAllBookings() {
+        if (bookings.isEmpty()) {
+            System.out.println("No bookings scheduled.\n");
+            return;
+        }
+
+        System.out.println("----- Bookings -----");
+        for (Booking b : bookings) {
+            System.out.println(b);
+        }
+        System.out.println();
+    }
+
+    public boolean deleteBooking(String clientName) {
+        return bookings.removeIf(b -> b.getClientName().equalsIgnoreCase(clientName));
+    }
+}
